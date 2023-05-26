c06e096486413280cd7a5ea12895ac687127de2d
Wyatt <wnapier@bu.edu>
2023-05-25 20:59:55 -0400

    buttons are interactive with states

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
           Learn reaction speed. NYOOM!
         </a>
         <TestButton />
+        <TestButton />
       </header>
     </div>
   );
--- src/TestButton.js
+++ src/TestButton.js
@@ -1,6 +1,28 @@
+/** allows you to use states in component */
+import { useState } from "react"; 
+
 function TestButton() {
+    /** 
+     * count: current state
+     * setCount: function that lets you update it
+     * either can have any name
+     * initial value is the parameter of useState
+     * each render of the same component gets it own state
+     */
+    const [count, setCount] = useState(0);
+    
+    /**
+     * handler function is used to respond to events inside component
+     * make sure to not use parentheses --> passing it down, not callinng
+     */
+    function handleClick() {
+        /** count is 1 behind what I expected, like it updates after the alert */
+        setCount(count + 1)
+        alert('You\'ve clicked me ' + count + ' too many times!');
+    }
+
     return (
-        <button>I'm a <br /> button</button>
+        <button onClick={handleClick}>I'm a button<br />with {count} clicks</button>
     );
 }
 
