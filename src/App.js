94fbff8a0836602d3145b3eed583069bd2e531ca
Wyatt <wnapier@bu.edu>
2023-07-21 21:13:12 -0400

    added spotify logout functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import WyattProf from './WyattProf.jpg';
 import './App.css';
 import TestButton from './TestButton';
 
 function App() {
-  const CLIENT_ID="c77e293321794990a3dc1349b1502d75";
-  const REDIRECT_URI="http://localhost:3000";
-  const AUTH_ENDPOINT="https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE="token";
+  const CLIENT_ID = "c77e293321794990a3dc1349b1502d75";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  // useEffect and logout functions from https://dev.to/dom_the_dev/how-to-use-the-spotify-api-in-your-react-js-app-50pn
+  const [token, setToken] = useState("");
+  useEffect (() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  }
 
 
   return (
     <div className="App">
-      <div className="Spotify Login"> 
+      <div className="Spotify Login/Logout"> 
         <h1>Spotify + React</h1>
-        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+            Login to Spotify</a>
+            : <button onClick={logout}>Logout</button>
+        }
       </div>
 
 
