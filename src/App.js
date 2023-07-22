1f93994d1791747fcfe2119662ed8977e39bdf42
Wyatt <wnapier@bu.edu>
2023-07-21 21:01:46 -0400

    added functionality for spotify authentification

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 import TestButton from './TestButton';
 
 function App() {
+  const CLIENT_ID="c77e293321794990a3dc1349b1502d75";
+  const REDIRECT_URI="http://localhost:3000";
+  const AUTH_ENDPOINT="https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE="token";
+
+
   return (
     <div className="App">
+      <div className="Spotify Login"> 
+        <h1>Spotify + React</h1>
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+      </div>
+
+
+
+
       <header className="App-header">
         {/** <image src={WyattProf} height="200" width="200" /> */}
         <img src={logo} className="App-logo" alt="logo" />
@@ -30,4 +44,7 @@ function App() {
 
 export default App;
 
-/** activated format on save */
+/** 
+ * activated format on save 
+ * run code in terminal with yarn start
+*/
